4cbc4073ae6010a26c7d8f12895dcdf21264310d
andretipolt <andretipoltlopes@gmail.com>
2023-12-20 16:52:40 -0300

    Showing data on edit course

--- Angular-Project-Front-End/src/app/courses/course-form/course-form.component.ts
+++ Angular-Project-Front-End/src/app/courses/course-form/course-form.component.ts
@@ -3,6 +3,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, NonNullableFormBuilder } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { CoursesService } from '../services/courses.service';
+import { ActivatedRoute } from '@angular/router';
+import { Course } from '../models/course';
 
 @Component({
   selector: 'app-course-form',
@@ -13,19 +15,27 @@ export class CourseFormComponent implements OnInit {
 
   form: FormGroup = this.formBuilder.group({
 
+    _id: new FormControl<string | null>(''),
     name: new FormControl<string | null>('', { nonNullable: true }),
-    category: new FormControl<string | null>('', { nonNullable: true })
+    category: new FormControl<string | null>('', { nonNullable: true }),
   });
 
   constructor(private formBuilder: NonNullableFormBuilder,
     private courseService: CoursesService,
     private _snackBar: MatSnackBar,
-    private location: Location) {
+    private location: Location,
+    private route: ActivatedRoute) {
   }
 
   ngOnInit(): void {
 
-    this.form.value
+    const course: Course = this.route.snapshot.data['course']; // Pegando o course do resolver que vai vai vir vazio ou nao
+    
+    this.form.setValue({
+      _id: course._id,
+      name: course.name,
+      category: course.category
+    })
   }
 
   onSubmit() {
--- Angular-Project-Front-End/src/app/courses/services/courses.service.ts
+++ Angular-Project-Front-End/src/app/courses/services/courses.service.ts
@@ -17,7 +17,7 @@ export class CoursesService {
 
     return this.httpClient.get<Course[]>(this.API).pipe(
         first(),
-        tap((courses) => console.log(courses))
+        tap()
       );
   }
 
