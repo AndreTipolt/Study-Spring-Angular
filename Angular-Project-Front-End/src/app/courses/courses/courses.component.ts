1efd51d838a84475e5630fa73db638c415c4d050
andretipolt <andretipoltlopes@gmail.com>
2023-12-20 12:04:41 -0300

    Creating button in table of add, edit and delete

--- Angular-Project-Front-End/src/app/courses/courses/courses.component.ts
+++ Angular-Project-Front-End/src/app/courses/courses/courses.component.ts
@@ -4,6 +4,7 @@ import { Course } from '../models/course';
 import { CoursesService } from '../services/courses.service';
 import { MatDialog } from '@angular/material/dialog';
 import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-courses',
@@ -14,8 +15,8 @@ export class CoursesComponent implements OnInit {
 
   courses$: Observable<Course[]>;
 
-  displayedColumns = ['_id','name', 'category']
-  constructor(private coursesServices: CoursesService, private dialog: MatDialog) {
+  displayedColumns = ['_id','name', 'category', 'actions']
+  constructor(private coursesServices: CoursesService, private dialog: MatDialog, private router: Router, private route: ActivatedRoute) {
     this.courses$ = this.coursesServices.list().pipe(
       catchError((error) => {
         this.onError("Erro ao carregar cursos");
@@ -32,6 +33,13 @@ export class CoursesComponent implements OnInit {
     this.dialog.open(ErrorDialogComponent, {
       data: errorMessage
     })
+  }
+
+  onAdd(){
+
+    // this.router.navigate(['courses/new']); // mesma coisa que isso aqui
+
+    this.router.navigate(['new'], {relativeTo: this.route })
   }
 
 }
