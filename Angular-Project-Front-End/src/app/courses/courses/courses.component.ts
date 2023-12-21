5b8af9e46361145d456420de6dfdccdb923bc6f9
andretipolt <andretipoltlopes@gmail.com>
2023-12-21 10:06:23 -0300

    Messagem form to sure that user wants to delete these course

--- Angular-Project-Front-End/src/app/courses/courses/courses.component.ts
+++ Angular-Project-Front-End/src/app/courses/courses/courses.component.ts
@@ -6,6 +6,7 @@ import { MatDialog } from '@angular/material/dialog';
 import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
 import { ActivatedRoute, Router } from '@angular/router';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { ConfirmationDialogComponent } from 'src/app/shared/confirmation-dialog/confirmation-dialog.component';
 
 @Component({
   selector: 'app-courses',
@@ -16,13 +17,18 @@ export class CoursesComponent implements OnInit {
 
   courses$!: Observable<Course[]>;
 
-  displayedColumns = ['_id','name', 'category', 'actions']
-  constructor(private coursesServices: CoursesService, private dialog: MatDialog, private router: Router, private route: ActivatedRoute, private matSnackBar: MatSnackBar) {
-    
+  displayedColumns = ['_id', 'name', 'category', 'actions']
+  constructor(private coursesServices: CoursesService,
+    private dialog: MatDialog,
+    private router: Router,
+    private route: ActivatedRoute,
+    private matSnackBar: MatSnackBar,
+  ) {
+
     this.refresh();
   }
 
-  refresh(){
+  refresh() {
 
     this.courses$ = this.coursesServices.list().pipe(
       catchError((error) => {
@@ -36,32 +42,42 @@ export class CoursesComponent implements OnInit {
 
   }
 
-  onError(errorMessage: string){
+  onError(errorMessage: string) {
     this.dialog.open(ErrorDialogComponent, {
       data: errorMessage
     })
   }
 
-  onAdd(){
+  onAdd() {
 
     // this.router.navigate(['courses/new']); // mesma coisa que isso aqui
 
-    this.router.navigate(['new'], {relativeTo: this.route })
+    this.router.navigate(['new'], { relativeTo: this.route })
   }
 
-  onEdit(course: Course){
+  onEdit(course: Course) {
 
-    this.router.navigate(['edit', course._id], {relativeTo: this.route })
+    this.router.navigate(['edit', course._id], { relativeTo: this.route })
   }
 
-  onDelete(id: string){
+  onDelete(id: string) {
 
-    this.coursesServices.delete(id).subscribe(() => {
-      this.refresh()
+    const dialogRef = this.dialog.open(ConfirmationDialogComponent, {
+      width: '250px',
+      data: "Deseja remover o curso?",
+    });
 
-      this.matSnackBar.open("Curso Removido com Sucesso")
-    }, (error) => {
-      this.onError("Erro ao remover curso")
-    })
+    dialogRef.afterClosed().subscribe((result: boolean) => {
+
+      if (result) {
+        this.coursesServices.delete(id).subscribe(() => {
+          this.refresh()
+
+          this.matSnackBar.open("Curso Removido com Sucesso")
+        }, (error) => {
+          this.onError("Erro ao remover curso")
+        })
+      }
+    });
   }
 }
--- Angular-Project-Front-End/src/app/shared/shared.module.ts
+++ Angular-Project-Front-End/src/app/shared/shared.module.ts
@@ -3,16 +3,18 @@ import { CommonModule } from '@angular/common';
 import { ErrorDialogComponent } from './components/error-dialog/error-dialog.component';
 import { AppMaterialModule } from './app-material/app-material.module';
 import { CategoryPipe } from './pipes/category.pipe';
+import { ConfirmationDialogComponent } from './confirmation-dialog/confirmation-dialog.component';
 
 @NgModule({
   declarations: [
     ErrorDialogComponent,
-    CategoryPipe
+    CategoryPipe,
+    ConfirmationDialogComponent
   ],
   imports: [
     CommonModule,
     AppMaterialModule
   ],
-  exports: [ErrorDialogComponent, CategoryPipe]
+  exports: [ErrorDialogComponent, CategoryPipe, ConfirmationDialogComponent]
 })
 export class SharedModule { }
